e42ff01e8117e54ff3c8b0f6d71dc5d05e503ab0
Aayush <halwaiaayush@gmail.com>
2026-01-05 14:28:18 +0545

    done js

---
+++ index.js
@@ -0,0 +1,54 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random()*max);
+}
+
+let computerValue= (getRandomInt(3));
+
+let userValue = parseInt(prompt("Enter 0=scissors 1=rock  2=Paper"));
+
+// 0 = scissors 1=rock  2= Paper
+if(computerValue === userValue){
+    console.log("Draw");
+    
+}
+
+else if((computerValue === 0 || userValue === 1)||
+    (computerValue === 1 || userValue ===2 )||
+    (computerValue === 2 || userValue === 0)
+){
+    console.log("User Wins");
+}
+else {
+    console.log("Computer WIns");
+    
+}
+
+// for output 
+let valueNameComputer ;
+if (computerValue===0){
+    valueNameComputer = "Scissors" ;
+}
+else if (computerValue===1){
+    valueNameComputer = "Rock" ;
+}
+else 
+{
+    valueNameComputer==="Paper"
+}
+console.log("Computer's pick = "+ valueNameComputer);
+
+let valueNameUser ;
+if (userValue===0){
+    valueNameUser = "Scissors" ;
+}
+else if (userValue===1){
+    valueNameUser = "Rock" ;
+}
+else 
+{
+    valueNameUser==="Paper"
+}
+console.log(`User's Pick = ${valueNameUser}`);
+
+
+
