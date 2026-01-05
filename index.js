7974cfd63a4491338e789387807299464c5d4eef
Aayush <halwaiaayush@gmail.com>
2026-01-05 15:06:46 +0545

    fixed colour

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+        const computerBtn = document.querySelector('.computer-btn');
+        const userBtns = document.querySelectorAll('.user-btn');
+        const gameInfoPara = document.getElementById('game-info-para');
+        const gameWin = document.getElementById('game-win');
+        const gameDraw = document.getElementById('game-draw');
+        const gameLoss = document.getElementById('game-loss');
+
+
 function getRandomInt(max) {
   return Math.floor(Math.random()*max);
 }
 
 let computerValue= (getRandomInt(3));
 
-let userValue = parseInt(prompt("Enter 0=scissors 1=rock  2=Paper"));
+let userValue = parseInt(ppt("Enter 0=scissors 1=rock  2=Paper"));
 
 // 0 = scissors 1=rock  2= Paper
 if(computerValue === userValue){
@@ -46,7 +54,7 @@ else if (userValue===1){
 }
 else 
 {
-    valueNameUser==="Paper"
+    valueNameUser="Paper"
 }
 console.log(`User's Pick = ${valueNameUser}`);
 
